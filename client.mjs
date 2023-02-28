f4c50d7b2915940add75702ce05d9d51c834b933
Pawel Motyl <pawmot@gmail.com>
2023-02-28 13:52:51 +0100

    Refactor client to avoid shared state

--- client.mjs
+++ client.mjs
@@ -1,18 +1,18 @@
 import * as net from 'net';
 
-let socket;
-let lines = [];
-let buffer = "";
-
 export const connect = async (host = 'localhost', port = 3000) => {
+  let socket;
+  let lines = [];
+  let buffer = "";
+
   return new Promise((resolve) => {
     socket = new net.Socket();
     socket.setNoDelay(true);
     socket.setEncoding('ascii');
     socket.connect(port, host, () => {
       resolve({
-        send,
-        readLine
+        send: send(socket),
+        readLine: readLine(lines)
       });
     });
 
@@ -25,28 +25,28 @@ export const connect = async (host = 'localhost', port = 3000) => {
       buffer += data;
       if (buffer.includes("\n")) {
         const parts = buffer.split("\n");
-        lines = lines.concat(parts.slice(0, parts.length - 1));
+        lines.push(...parts.slice(0, parts.length - 1));
         buffer = parts[parts.length - 1];
       }
     });
   });
-}
+};
 
-const send = (msg) => {
+const send = (socket) => (msg) => {
   const promise = new Promise((resolve) => {
     socket.write(`${msg}\n`, 'ascii', () => {
       resolve();
     });
   });
   return promise;
-}
+};
 
-const readLine = async () => {
+const readLine = (lines) => async () => {
   while (lines.length === 0) {
     await delay(5);
   }
   return lines.shift();
-}
+};
 
 const delay = async (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms))
