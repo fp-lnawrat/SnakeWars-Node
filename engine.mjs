75983ef56ac6ad5e8a2e72f8caea5af077a5ff2f
lnawrat <lnawrat@future-processing.com>
2023-02-28 17:03:04 +0100

    Debugging the connection issue.

--- engine.mjs
+++ engine.mjs
@@ -1,25 +1,27 @@
 import { connect } from './client.mjs';
 import { stopwatch } from './stopwatch.mjs';
 
-const PLAYER = 'PL2';
-const HOST = 'localhost';
-const PORT = 3000;
+const PLAYER = 'BARBI';
+const HOST = 'FP-LSC0248';
+const PORT = 9977;
 
 export const start = async (nextMove) => {
   console.log('Starting SnakeWars bot');
-  const {send, readLine} = await connect(HOST, PORT);
+  const { send, readLine } = await connect(HOST, PORT);
   console.log('Connected to SnakeWars');
-  const hello = await readLine();
-  if (hello !== 'ID') {
-    console.log('Expected "ID" from server, got', hello);
-    process.exit(1);
+  const hello = await readLine(); 
+  if (hello != 'ID') {
+    console.log('Expected "ID" from server, got', '->' + hello + '<-');
+    // process.exit(1);
   }
 
+  console.log('debug 001');
   await send(PLAYER);
+  console.log('debug 002');
   const id = await readLine();
+  console.log('debug 003');
 
   console.log('Got ID', id);
-  
   while(true) {
     const sw = stopwatch();
     const gameState = JSON.parse(await readLine());
